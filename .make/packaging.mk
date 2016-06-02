7a1e747cc9ce411c30f2a0a89122aea5c128d46f
warmans <stefan.warman@gmail.com>
2016-06-02 19:20:40 +0200

    Update packaging make include

--- .make/packaging.mk
+++ .make/packaging.mk
@@ -1,25 +1,29 @@
-PACKAGE_VERSION ?=
+
+# Version identifier to use for package. This should be something like 1.0.0 (REQUIRED)
+PACKAGE_VERSION ?= $(RELEASE_VERSION)
+
+# Location to stage the package contents before creating the archive. (OPTIONAL)
 PACKAGE_CONTENT_DIR ?= .packaging
+
+# Where to put the artifact (deb or whatever) (OPTIONAL)
 PACKAGE_OUTPUT_DIR ?= .
-PACKAGE_TYPE ?= deb
+
+# Type of package to build. This is passed directly to fpm.
+PACKAGE_TYPE ?= deb (OPTIONAL)
+
+# The package name (REQUIRED)
 PACKAGE_NAME ?=
 
 .PHONY: package
-package:
+package: _configure_package
 
 	@if [ -z "$(shell which fpm 2>/dev/null)" ]; then \
 		echo "error:\nPackaging requires effing package manager (fpm) to run.\nsee https://github.com/jordansissel/fpm\n"; \
 		exit 1; \
 	fi
 
-	#run make install against the packaging dir
-	mkdir -p ${PACKAGE_CONTENT_DIR} && $(MAKE) _configure_package
-
-	mkdir -p ${PACKAGE_OUTPUT_DIR}
-
 	#build package
-	fpm \
-		--rpm-os linux \
+	fpm --rpm-os linux \
 		--force \
 		-s dir \
 		-p ${PACKAGE_OUTPUT_DIR} \
@@ -27,3 +31,7 @@ package:
 		-n ${PACKAGE_NAME} \
 		-v ${PACKAGE_VERSION} \
 		-C ${PACKAGE_CONTENT_DIR} .
+
+.PHONY: _configure_package
+_configure_package:
+	#you must override this with something that adds the relevant files to $(PACKAGE_CONTENT_DIR)
